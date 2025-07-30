14425de5d054992b9e29afb2d7eedda02fab587b
Abhinav Singh <130390719+singhAbhina@users.noreply.github.com>
2025-07-30 23:52:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-}));
 
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist'  
+    outDir: 'dist',   // Output directory for Render
+    chunkSizeWarningLimit: 2000
   }
 });
